8fb30d2be74c1a5c272e64efbfcb3c827ee4da15
Sa18-f <sisazid182713@gmail.com>
2024-05-16 22:07:40 +0600

    cookie Options added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', "https://car-doctor-4f8b8.web.app", "https://car-doctor-4f8b8.firebaseapp.com"],
     credentials: true
 }));
 app.use(express.json());
@@ -50,6 +50,12 @@ const verifyToken = async (req, res, next) => {
     })
 }
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -63,15 +69,12 @@ async function run() {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: false,
-            }).send({ success: true })
+            res.cookie('token', token, cookieOptions).send({ success: true })
         })
         // logout user
         app.post('/logout', async (req, res) => {
             const user = req.body;
-            res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+            res.clearCookie('token', { ...cookieOptions, maxAge: 0 }).send({ success: true });
         })
 
 
